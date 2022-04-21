43d1d365db0c7ac8b168e98e99f9739f3a9adae1
Hemanth3004 <104094408+Hemanth3004@users.noreply.github.com>
2022-04-21 11:21:58 +0530

    Add files via upload

---
+++ Java_Pgm/src/java1/days.java
@@ -0,0 +1,14 @@
+package java1;
+
+public class days {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           int a=10;
+           int b=20;
+           int c=a+b;
+           System.out.Println("The total sum is " +c);
+        		  
+	}
+
+}
